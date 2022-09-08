408a3d12f55e35ec380022cdc922549706c8fe24
Dawid <stacheradawid@wp.pl>
2022-09-08 14:41:57 +0200

    master

--- theme-swap.js
+++ theme-swap.js
@@ -1,6 +1,35 @@
 const button = document.getElementById("theme-swap");
-button.addEventListener("click", changeTheme);
+button.addEventListener("click", () => {
 
-function changeTheme(){
-    console.log('change theme')
-}
+    const elements = document.getElementsByTagName('html');
+    const html = elements[0];
+
+    const theme = getTheme();
+    html.classList.toggle('light', isThemeLight)
+
+    let isThemeLight = theme === 'light';
+
+    if(isThemeLight){
+        html.classList.remove('light')
+        button.innerText = "🌞";
+        setTheme('dark');
+    } else{
+        html.classList.add('light');
+        button.innerText = "🌑";
+        setTheme('light');
+    }
+});
+
+function getTheme(){
+    const theme = localStorage.getItem('theme');
+
+    if('theme' === 'light'){
+        return 'light';
+    } else{
+        return 'dark';
+    } 
+};
+
+function setTheme(){
+    localStorage.setItem('theme', theme);
+};
